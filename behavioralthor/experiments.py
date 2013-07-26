283d714f96f0823d3a483ee689a4971fea05910b
Diego Ardila <ardila@mit.edu>
2013-07-26 14:52:32 -0400

    Dropping L1R

--- behavioralthor/experiments.py
+++ behavioralthor/experiments.py
@@ -2,22 +2,24 @@ __author__ = 'headradio'
 from devthor.procedures import suggest_multiple_from_name
 
 
-def fruits_vs_chairs(dbname='fruits_vs_chairs', randomSearch=False):
+def fruits_vs_chairs(dbname='diego_fruits_vs_chairs', randomSearch=False):
     host = 'localhost'
     port = 22334
     bandit_names = ['behavioralthor.bandits.object_level_fruits_and_chairs_L3',
                     # 'behavioralthor.bandits.fruits_vs_chairs_L3',
                     'behavioralthor.bandits.fruits_vs_chairs_L2',
-                    'behavioralthor.bandits.fruits_vs_chairs_L4',
-                    'behavioralthor.bandits.fruits_vs_chairs_L1R']
+                    'behavioralthor.bandits.fruits_vs_chairs_L4'
+                    #'behavioralthor.bandits.fruits_vs_chairs_L1R'
+                    ]
     nExps = len(bandit_names)
     bandit_args_list = [() for _i in range(nExps)]
     bandit_kwargs_list = [{} for _i in range(nExps)]
     exp_keys = ['obj_L3',
                 # 'L3',
                 'L2',
-                'L4',
-                'L1R']
+                'L4'
+                #'L1R'
+                ]
     if randomSearch:
         bandit_algo_names = ['hyperopt.Random'] * nExps
         bandit_algo_args_list = [() for _i in range(nExps)]
