dd9be47d4fe04e8f8a04c4cad64eb72dcfa74d34
Diego Ardila <ardila@mit.edu>
2013-12-16 17:12:52 -0500

    Script used for evaluating features on all 2-ways

--- behavioralthor/feature_screening_big.py
+++ behavioralthor/feature_screening_big.py
@@ -2,6 +2,7 @@ __author__ = 'headradio'
 from behavioralthor.utils import bigtrain
 import imagenet
 from collections import defaultdict
+import cPickle
 #from dldata.metrics.utils import get_subset_splits
 import numpy as np
 
@@ -13,15 +14,15 @@ import numpy as np
 
 label_filename = 'not_yet'
 
-y = np.load(label_filename)
+y = cPickle.load('/home/ardila/synsets.p')
 #This was too slow
 #splits = get_subset_splits(dataset.meta, npc_train=150, npc_tests=[50], num_splits=1, catfunc=lambda x: x['synset'])
 #split = splits[0][0]
 
 count = {}
 split = defaultdict(list)
-npctrain = 300
-npctest = 100
+npctrain = 400
+npctest = 200
 for i in range(y.shape[0]):
     count[y[i]] = count.get(y[i], 0) + 1
     if i % 1000 == 0:
@@ -32,9 +33,8 @@ for i in range(y.shape[0]):
         split['test'].append(i)
 
 
-
-screen_name = 'BIG_PIXEL'
-F = dataset.get_pixel_features(preproc)
+screen_name = 'CHALLENGE_FEATURE'
+F = np.memmap('/home/ardila/features.dat', dtype='float32', mode='w+', shape=(1200256, 8192))
 Xtrain = np.memmap(filename='train_memmap_'+str(screen_name)+'.dat',
                    dtype='float32', mode='w+', shape=(len(split['train']), 4096*2))
 Xtest = np.memmap(filename='test_memmap_'+str(screen_name)+'.dat',
@@ -54,10 +54,10 @@ chunk_size = 200000
 
 for chunks1, chunks2 in zip(chunks(range(Xtrain.shape[0]), chunk_size), chunks(split['train'], chunk_size)):
     print float(max(chunks1))/Xtrain.shape[0]
-    Xtrain[chunks1] = P[chunks2]
+    Xtrain[chunks1] = F[chunks2]
 for chunks1, chunks2 in zip(chunks(range(Xtest.shape[0]), chunk_size), chunks(split['test'], chunk_size)):
     print float(max(chunks1))/Xtrain.shape[0]
-    Xtest[chunks1] = P[chunks2]
+    Xtest[chunks1] = F[chunks2]
 
 ytrain = y[split['train']]
 ytest = y[split['test']]
--- feature_saving_script.py
+++ feature_saving_script.py
@@ -6,23 +6,25 @@ import numpy as np
 import cStringIO
 feature_dir1 = '/diego_features/diego_features/a'
 feature_dir2 = '/diego_features/diego_features/b'
-data_dir = '/share/export/batch_storage2/batch128_img138_full'
-files1 = os.listdir(feature_dir1)
-files2 = os.listdir(feature_dir2)
-data_files = [f for f in os.listdir(data_dir) if ~f.endswith('.meta')]
+files1 = [f for f in os.listdir(feature_dir1) if f.startswith('data_batch')]
+files2 = [f for f in os.listdir(feature_dir2) if f.startswith('data_batch')]
 fs = imagenet.dataset.get_img_source()
 extraction_name = 'challenge_set_v0'
-for f1, f2, df in zip(files1, files2, data_files)[:2]:
+fp = np.memmap('features.dat', dtype='float32', mode ='w+', shape=(1200256, 8192))
+i = 1
+for f1, f2 in zip(files1, files2):
     print f1
-    print df
+    print float(i)/1200256
     fb1 = cPickle.load(open(os.path.join(feature_dir1, f1), 'rb'))['data']
     fb2 = cPickle.load(open(os.path.join(feature_dir2, f2), 'rb'))['data']
     features = np.concatenate([fb1, fb1], axis=1)
-    db = cPickle.load(open(os.path.join(data_dir, df), 'rb'))
-    filenames = db['filenames']
-    tempfile = cStringIO.StringIO()
-    for feature, filename in zip(features, filenames):
-        np.save(tempfile, feature)
-        name = filename+extraction_name
-        print name
-        fs.put(tempfile, _id=name)
+    fp[i:i+128, :] = features
+    i += 128
+    # db = cPickle.load(open(os.path.join(data_dir, df), 'rb'))
+    # filenames = db['filenames']
+    # tempfile = cStringIO.StringIO()
+    # for feature, filename in zip(features, filenames):
+    #     np.save(tempfile, feature)
+    #     name = filename+extraction_name
+    #     if not fs.exists(name):
+    #         fs.put(tempfile, _id=name)
